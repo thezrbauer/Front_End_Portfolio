3a6a002640470009e6c6c626f169d8d439606d65
Zack Bauer <zack.bauer30@gmail.com>
2022-03-23 16:44:07 -0400

    grid area

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,36 +1,36 @@
 
 import './App.css';
-
+import hero from "./images/hero.JPG"
 function App() {
   return (
     <div className="App">
       <div>
       <header>
         <div className="nav">
-          <a className="nav-itm" id="about">About</a>
+          <a className="nav-itm" id="about" href="#about-section">About</a>
           <a className="nav-itm" id="projects">Projects</a>
           <a className="nav-itm"id="contact">Contact</a>
           <a className="nav-itm"id="other">Resume</a>
         </div>
       </header>
       <div className="body">
-        <section className="sec">
-          <div className="hero-body">
-           <div className="hero-intro">
-             <div className="intro-text">
-              <h1>Hi I'm Zack. I am a Front End Web Designer with a passion for bringing his ideas to life.</h1>
+        <section className="section-a" id="about-section">
+          <div className="grid-area">
+           <div className="grid-1"></div>
+            <div className="grid-2">
+              <img src={hero} alt="Photo of subject"></img>
               </div>
-           <div className="hero-img">
-              <img alt="Photo of subject"></img>
-              </div>
-              </div>
-            <div className="hero-text">
+            <div className="grid-3">
               <p>Wee woo wee woo</p>
               <p>I am a Front End Web Designer. Looking to provide you with your dream project</p>
             </div>
+            <div className="grid-4">
+              <h1>Hi I'm Zack. I am a Front End Web Designer with a passion for bringing his ideas to life.</h1>
+            
+              </div>
           </div>
         </section>
-        <section className="sec-alt">
+        <section className="section-b">
           <div className="project-grid">
               <div className="grid-item">
               IP address Tracker
@@ -43,7 +43,7 @@ function App() {
              
           </div>
         </section>
-        <section className="sec">
+        <section className="section-a">
           <div className="contact">Contact Stuff With Icons</div>
         </section>
         <footer></footer>
