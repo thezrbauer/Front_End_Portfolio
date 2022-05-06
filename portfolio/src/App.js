b7619818a74902f2adee1e21d2b1a00d80106a73
Zack Bauer <zack.bauer30@gmail.com>
2022-05-06 04:35:20 -0400

    workin

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -17,7 +17,7 @@ function App() {
         <div className="nav">
           <a className="nav-itm" id="about" href="#about-section">About</a>
           <a className="nav-itm" id="projects" href="#project-section">Projects</a>
-          <a className="nav-itm"id="contact">Contact</a>
+          <a className="nav-itm"id="contact"href="#contact-section">Contact</a>
     
         </div>
       </header>
@@ -30,17 +30,47 @@ function App() {
               </div>
             <div className="grid-3">
               <p>I love to code! I taught myself to build websites for fun, but quickly developed a passion for identifying and solving problems, 
-                and being able to create something Amazing from some lines of code. Coding gives me the chance to express myself,
-                 and now I want to share my work with you </p>
+                and using that to create something amazing out of code. Coding has given me the chance to express myself, and has motivated me to 
+                continue growing as a developer and to ultimately break into the field.  </p>
           
             </div>
             <div className="grid-4">
-              <h1>Hi I'm Zack. I am a Front End Web Designer, check out what I have made!</h1>
+              <h1>Hi I'm Zack. I am a Front End Web Developer and Designer, check out what I have made!</h1>
             
               </div>
           </div>
         </section>
-        <section className="section-b" id="project-section">
+
+        <section className="section-b">
+          <div className="skills">
+            <h2>These are some of the things I've learned how to use</h2>
+            </div>
+          <div className="known-icons">
+          <div className="icons"><FontAwesomeIcon icon={faHtml5} fixedWidth size="2xl"/> 
+            <h4>HTML</h4>
+            </div>
+            <div className="icons"><FontAwesomeIcon icon={faJs} fixedWidth size="2xl"/> 
+            <h4>JavaScript</h4>
+            </div>
+            <div className="icons"><FontAwesomeIcon icon={faCss3} fixedWidth size="2xl"/> 
+            <h4>CSS3</h4>
+            </div>
+            <div className="icons"><FontAwesomeIcon icon={faNode} fixedWidth size="2xl"/> 
+            <h4>NodeJS</h4>
+            </div>
+            <div className="icons"><FontAwesomeIcon icon={faGit} fixedWidth size="2xl"/> 
+            <h4>git</h4>
+            </div>
+            <div className="icons"><FontAwesomeIcon icon={faReact} fixedWidth size="2xl"/> 
+            <h4>React</h4>
+            </div>
+            </div>
+               
+     
+          
+        </section>
+
+        <section className="section-a" id="project-section">
           <div className="project-intro">
             <h2>Here are some of my Recent Projects</h2>
             <p></p>
@@ -48,66 +78,48 @@ function App() {
           <div className="project-grid">
               <div className="grid-item grd-ip">
               <h3 className="grid-title">IP address Tracker</h3>
+              <button className="btn"> View Code</button>
               </div>
 
               <div className="grid-item grd-gall">
                 <h3 className="grid-title">Galleria</h3>
+                <button className="btn" onClick={(e) => {
+                  e.preventDefault();
+                  window.location.href='https://github.com/thezrbauer/galleria';
+                }}> View Code</button>
                 </div>
 
               <div className="grid-item grd-time">
                 <h3 className="grid-title">Time Tracker</h3> 
-                {/* <button>Visit the Site</button>
-                <button>Look at the code</button> */}
+                <button className="btn">View Code</button>
                 </div>
                 
               <div className="grid-item grd-calc">
                 <h3 className="grid-title">Calculator</h3>
+                <button className="btn"> View Code</button>
                 </div>
 
               <div className="grid-item grd-ec">
               <h3 className="grid-title">E-Commerce</h3>
+              <button className="btn"> View Code</button>
               </div>
 
               <div className="grid-item grd-book">
-              <h3 className="grid-title">Bookmark Landing page
-              </h3></div>
+              <h3 className="grid-title">Bookmark Landing page</h3>
+              <button className="btn"> View Code</button>
+              </div>
              
           </div>
         </section>
-        <section className="section-a">
-          <div className="contact">
-            <h2>This is what I know how to use</h2>
-            </div>
-
-          <div className="known-icons">
-            <div><FontAwesomeIcon icon={faReact} fixedWidth size="2xl"/> 
-            <h4>React</h4>
-            </div>
-            <div><FontAwesomeIcon icon={faJs} fixedWidth size="2xl"/> 
-            <h4>JavaScript</h4>
-            </div>
-            <div><FontAwesomeIcon icon={faCss3} fixedWidth size="2xl"/> 
-            <h4>CSS3</h4>
-            </div>
-            <div><FontAwesomeIcon icon={faHtml5} fixedWidth size="2xl"/> 
-            <h4>HTML</h4>
-            </div>
-            <div><FontAwesomeIcon icon={faNode} fixedWidth size="2xl"/> 
-            <h4>NodeJS</h4>
-            </div>
-            <div><FontAwesomeIcon icon={faGit} fixedWidth size="2xl"/> 
-            <h4>NodeJS</h4>
-            </div>
-            </div>
-               
-            <h2>This is how to contact me</h2>
+    
+        <section className="section-c">
+          <div className="contact" id="contact-section">
+        <h2>Click below and say Hi!</h2>
+        </div>
             <div className="contact-icons"> 
                 <FontAwesomeIcon icon={faGithub} size="2xl"/>
                 </div>
-          
-        </section>
-
-        
+                </section>
         <footer></footer>
       </div>
       </div>
